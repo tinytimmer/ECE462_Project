d5c27edd1f39c473db98837222685daba509b118
underscoreXY <149117786+underscoreXY@users.noreply.github.com>
2024-12-07 17:18:19 -0700

    Add files via upload

--- BHT.v
+++ BHT.v
@@ -1,7 +1,7 @@
-module BHT(bht, clk, index, isitbranch, itisbranch, prediction, Address, Target, branch, instruction, targetpc, indexdec, indexout);
+module BHT(bht,clk,index,isitbranch,update,itisbranch, prediction, Address, Target, branch,instruction,targetpc,indexdec,indexout);
 input [3:0] index; //[2:0]PC. Carried by pipeline to next stage
 input [3:0] indexdec; //Index from decode
-input branch, isitbranch, itisbranch, clk; //Real result (branch), checks if its a branch or not
+input branch, isitbranch,clk; //Real result (branch)
 input [31:0] Address; //PC
 input [5:0] instruction;
 reg [31:0] BHT [7:0];
@@ -13,7 +13,8 @@ input [31:0] Target; //Target PC
 output reg prediction;
 output reg [31:0] targetpc;
 output reg [3:0] indexout;
-output reg itisbranch; //carried by pipeline, tells us if previous instruciton was a branch confirming its a branch
+output reg itisbranch; //carried by pipeline
+output reg update;
 output [31:0] bht;
 assign bht = BHT[index];
 integer i;
@@ -32,7 +33,7 @@ always @(posedge clk)
 begin
 if (instruction == 4 || instruction == 5 || instruction == 6 || instruction == 7)
 begin
-itisbranch <= 1; //if its a branch instr, go to it
+itisbranch <= 1;
 if (BHT[index] != Address)
 begin
 BHT[index] <= Address;
@@ -43,7 +44,7 @@ if ( isitbranch == 1)
 begin
 if (branch != predictions[indexdec])
 begin
-if (predictions[indexdec] == 1) //a tally
+if (predictions[indexdec] == 1)
 predictions[indexdec] <= predictions[indexdec] - 1;
 else
 predictions[indexdec] <= predictions[indexdec] + 1;
@@ -59,8 +60,8 @@ begin
 if (instruction == 4 || instruction == 5 || instruction == 6 || instruction == 7)
 begin 
 
-if (BHT[index] == Address)
-begin                                     
+if (BHT[index] == Address)      //Fix: New insturctions shouldn't inherit predictions of previous
+begin                                     // instructions with the same index
 prediction <= predictions[index];
 targetpc <= Targets[index];
 indexout <= index;
